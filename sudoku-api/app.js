4978c308c368c4c654a8d61b275b0fe7e8d97cc3
Marcelo Soffredi <msoffredi@msoffredi@gmail.com>
2021-08-19 18:33:35 -0500

    Added mock data and activated it when in local environment

--- sudoku-api/app.js
+++ sudoku-api/app.js
@@ -1,53 +1,54 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB();
 
-const games = {
-    easy1: [
-        [null, null, null, null, 9, null, 8, 7, 2],
-        [7, null, 2, null, 8, 1, 4, 5, null],
-        [9, null, null, 2, null, null, null, 1, 3],
-        [1, null, null, 9, null, null, null, 4, null],
-        [null, 7, null, 1, null, 5, null, null, null],
-        [4, 2, null, null, null, 8, 5, 6, null],
-        [null, 3, 5, null, 8, null, null, null, null],
-        [8, null, 4, null, 3, 6, 5, null, null],
-        [null, 9, 6, 7, null, null, null, 3, 2],
+const localResponse = {
+    Items: [
+        {
+            puzzle: {
+                S: '[[null,7,null,null,null,8,null,null,2],[null,3,5,7,1,null,9,4,8],[8,1,2,3,null,null,null,7,null],[null,5,6,null,null,null,8,null,1],[3,9,null,null,8,null,null,null,7],[null,null,null,1,null,null,null,null,4],[null,null,null,null,8,5,null,null,null],[null,5,null,null,6,3,1,7,4],[7,null,null,null,null,null,5,3,null]]',
+            },
+            solution: {
+                S: '[[9,7,4,5,6,8,3,1,2],[6,3,5,7,1,2,9,4,8],[8,1,2,3,4,9,6,7,5],[4,5,6,2,9,7,8,3,1],[3,9,1,4,8,6,5,2,7],[2,8,7,1,5,3,9,6,4],[1,4,3,7,8,5,6,2,9],[8,5,9,2,6,3,1,7,4],[7,2,6,4,9,1,5,3,8]]',
+            },
+            id: { S: '2187' },
+        },
+        {
+            puzzle: {
+                S: '[[4,null,null,7,null,null,3,null,null],[null,null,1,4,5,null,8,7,null],[null,null,null,3,null,8,null,4,2],[9,4,2,1,null,null,6,7,null],[null,6,null,null,null,5,null,3,null],[7,3,5,null,2,4,null,9,null],[null,9,7,null,1,null,null,3,4],[null,null,null,9,8,null,null,null,7],[null,null,null,null,null,null,5,null,9]]',
+            },
+            solution: {
+                S: '[[4,6,8,7,2,1,3,5,9],[3,2,1,4,5,9,8,7,6],[9,5,7,3,6,8,1,4,2],[9,4,2,1,8,3,6,7,5],[1,6,8,7,9,5,2,3,4],[7,3,5,6,2,4,8,9,1],[8,9,7,5,1,6,2,3,4],[5,4,3,9,8,2,6,1,7],[2,1,6,4,7,3,5,8,9]]',
+            },
+            id: { S: '6470' },
+        },
+        {
+            puzzle: {
+                S: '[[null,1,null,2,null,null,8,4,5],[2,9,null,1,8,null,7,6,3],[null,null,5,6,7,4,null,null,2],[null,null,null,null,null,null,null,null,2],[null,null,8,5,4,2,3,null,6],[null,6,3,null,1,8,null,4,9],[3,6,null,null,2,9,4,5,8],[null,null,null,null,null,null,6,3,7],[null,5,7,null,8,6,null,null,null]]',
+            },
+            solution: {
+                S: '[[6,1,7,2,9,3,8,4,5],[2,9,4,1,8,5,7,6,3],[8,3,5,6,7,4,1,9,2],[5,7,4,9,3,6,1,8,2],[9,1,8,5,4,2,3,7,6],[2,6,3,7,1,8,5,4,9],[3,6,1,7,2,9,4,5,8],[8,2,9,4,5,1,6,3,7],[4,5,7,3,8,6,9,2,1]]',
+            },
+            id: { S: '4610' },
+        },
+        {
+            puzzle: {
+                S: '[[null,6,null,null,null,null,null,null,7],[5,7,2,null,8,null,1,null,6],[null,8,null,null,null,7,null,null,9],[null,1,null,null,null,null,null,null,2],[null,9,7,2,null,null,null,6,1],[2,null,8,9,null,null,null,null,4],[7,null,8,6,null,3,2,5,null],[null,null,null,null,null,null,7,null,null],[null,3,5,8,null,2,null,9,null]]',
+            },
+            solution: {
+                S: '[[4,6,9,1,2,5,3,8,7],[5,7,2,9,8,3,1,4,6],[3,8,1,6,4,7,5,2,9],[5,1,4,8,7,6,9,3,2],[3,9,7,2,5,4,8,6,1],[2,6,8,9,1,3,7,5,4],[7,4,8,6,9,3,2,5,1],[6,2,9,4,1,5,7,3,8],[1,3,5,8,7,2,4,9,6]]',
+            },
+            id: { S: '2166' },
+        },
+        {
+            puzzle: {
+                S: '[[4,9,null,5,6,null,null,8,null],[3,null,1,null,7,null,null,null,null],[7,8,6,null,null,9,4,null,3],[null,5,1,null,4,6,8,null,9],[null,4,2,null,null,null,6,null,null],[6,null,null,9,null,5,null,null,null],[6,7,null,3,null,null,null,null,null],[null,2,null,null,9,4,1,null,6],[null,9,1,5,null,null,null,null,4]]',
+            },
+            solution: {
+                S: '[[4,9,2,5,6,3,1,8,7],[3,5,1,4,7,8,2,6,9],[7,8,6,1,2,9,4,5,3],[7,5,1,2,4,6,8,3,9],[9,4,2,8,3,7,6,1,5],[6,3,8,9,1,5,2,4,7],[6,7,4,3,1,8,9,2,5],[5,2,3,7,9,4,1,8,6],[8,9,1,5,6,2,3,7,4]]',
+            },
+            id: { S: '1925' },
+        },
     ],
-    almostFinished1: [
-        [5, 1, 3, 6, 9, 4, 8, 7, 2],
-        [7, 6, 2, 3, 8, 1, 4, 5, 9],
-        [null, null, 4, 2, 5, 7, 6, 1, 3],
-        [1, 5, 8, 9, 2, 6, 3, 4, 7],
-        [6, 7, 3, 1, 4, 5, 2, 9, 8],
-        [4, 2, 9, 3, 7, 8, 5, 6, 1],
-        [7, 3, 5, 2, 8, 1, 4, 6, 9],
-        [8, 2, 4, 9, 3, 6, 5, 1, 7],
-        [1, 9, 6, 7, 4, 5, 8, 3, 2]
-    ],
-};
-
-const solutions = {
-    solutionEasy1: [
-        [5, 1, 3, 6, 9, 4, 8, 7, 2],
-        [7, 6, 2, 3, 8, 1, 4, 5, 9],
-        [9, 8, 4, 2, 5, 7, 6, 1, 3],
-        [1, 5, 8, 9, 2, 6, 3, 4, 7],
-        [6, 7, 3, 1, 4, 5, 2, 9, 8],
-        [4, 2, 9, 3, 7, 8, 5, 6, 1],
-        [7, 3, 5, 2, 8, 1, 4, 6, 9],
-        [8, 2, 4, 9, 3, 6, 5, 1, 7],
-        [1, 9, 6, 7, 4, 5, 8, 3, 2]
-    ],
-};
-
-const game1 = {
-    start: games.easy1,
-    solution: solutions.solutionEasy1
-};
-
-const game2 = {
-    start: games.almostFinished1,
-    solution: solutions.solutionEasy1
 };
 
 // We use capitalized name here to easy include in objects
@@ -59,12 +60,12 @@ const DefaultPageSize = 100;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
     let body;
@@ -74,36 +75,57 @@ exports.lambdaHandler = async (event, context) => {
     const unsopported = `Unsupported route: "${event.resource}" on ${event.httpMethod} method.`;
 
     const headers = {
-        "Content-Type": "application/json",
-        "access-control-allow-origin": "*"
+        'Content-Type': 'application/json',
+        'access-control-allow-origin': '*',
     };
 
     try {
         if (event.httpMethod === 'GET') {
             switch (event.resource) {
                 case '/puzzles/{start}':
-                    const start = event.pathParameters.start | '1';
-                    response = await ddb.scan({ 
-                        TableName,
-                        ExclusiveStartKey: { id: { S: start.toString() } },
-                        Limit: DefaultPageSize
-                    }).promise();
+                    if (process.env.AWS_LAMBDA_INITIALIZATION_TYPE) {
+                        const start = event.pathParameters.start | '1';
+                        response = await ddb
+                            .scan({
+                                TableName,
+                                ExclusiveStartKey: {
+                                    id: { S: start.toString() },
+                                },
+                                Limit: DefaultPageSize,
+                            })
+                            .promise();
+                    } else {
+                        response = localResponse;
+                    }
+
                     body = response.Items.map((item) => {
                         return {
                             puzzle: item.puzzle.S,
                             solution: item.solution.S,
-                            id: item.id.S
+                            id: item.id.S,
                         };
                     });
                     break;
+
                 case '/puzzles':
-                    response = await ddb.describeTable({ TableName }).promise();
+                    if (process.env.AWS_LAMBDA_INITIALIZATION_TYPE) {
+                        response = await ddb
+                            .describeTable({ TableName })
+                            .promise();
+                    } else {
+                        // Only on local environments, to avoid hitting DynamoDB
+                        response = { Table: { ItemCount: 12345 } };
+                    }
+
                     body = {
                         ItemCount: response.Table.ItemCount,
                         DefaultPageSize,
-                        Pages: Math.ceil(response.Table.ItemCount/DefaultPageSize)
-                    }
+                        Pages: Math.ceil(
+                            response.Table.ItemCount / DefaultPageSize,
+                        ),
+                    };
                     break;
+
                 default:
                     throw new Error(unsopported);
             }
@@ -121,6 +143,6 @@ exports.lambdaHandler = async (event, context) => {
     return {
         statusCode,
         body,
-        headers
+        headers,
     };
 };
